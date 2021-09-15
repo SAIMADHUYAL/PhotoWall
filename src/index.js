e811f076b635ced004abe0edafb80cd3cbdd15dc
SAIMADHUYAL <saimadhu.yal@gmail.com>
2021-09-15 11:41:31 -0500

    Creating diff components and Combining into one and passing the props into the element.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,70 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 
-const element = React.createElement('h1', null, 'Hello world');
-ReactDom.render(element, document.getElementById('root'));
+//const tasks = ['Take out the trash','Walk the Dog','How are you'];
+//const element = React.createElement('h1', null, 'Hello world');
+// const element = React.createElement('ol',null,
+// React.createElement('li',null,'Take out the trash'),
+// React.createElement('li',null,'Walk the Dog'),
+// React.createElement('li',null,'How are you')
+// );
+
+// const element = React.createElement('ol', null, tasks.map((task) => React.createElement('li', null, task)));
+// ReactDom.render(element, document.getElementById('root'));
+
+
+
+//React Elements.
+//For looping into each element in the array we use map().
+//Gave key = index for the unique props inside the element.
+// const element = 
+// <div>
+//   <h1>Task List</h1>
+//   <button>Click here</button>
+//   <ol>
+//     { tasks.map((task,index) => <li key = {index}>{task},{index}</li>)}
+//   </ol>
+// </div>
+
+// //For rendering the element we use ReactDom.render()
+// ReactDom.render(element,document.getElementById('root'));
+
+
+
+//React Components.
+//create a class.
+//We can create each element as a Component and combine all of them in single Main component to render 
+
+class List extends Component{
+
+  render(){
+    return(
+      <ul>
+        { this.props.tasks.map((task,index) => <li key = {index}>{task}</li>)}
+      </ul>
+    );
+  }
+}
+
+class Title extends Component{
+  render(){
+    return(
+      <h1>{this.props.title}</h1>
+    )
+  }
+}
+
+// We can render the Component any number of times inside another component.
+class Main extends Component{
+  render(){
+    return(
+      <div>
+        <Title title = {'Task Header'}/>
+        <List tasks = {['Mow the lawn','Walk the Dog']}/>
+        <List tasks = {['hose the driveway','finish the laundry']}/>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<Main/>,document.getElementById('root'));
